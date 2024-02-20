98c601fbdb528e0be0856c84237171b4d696c602
David Leal Peláez <61529718+Zares1331G@users.noreply.github.com>
2024-02-19 20:55:24 -0500

    Fix: Arreglo menu mobile

--- react/components/CloseSearchModal/index.tsx
+++ react/components/CloseSearchModal/index.tsx
@@ -1,14 +1,18 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDrawer } from 'vtex.store-drawer/DrawerContext'
 import { useRuntime } from 'vtex.render-runtime'
 
 const CloseSearchModal = () => {
   const { close } = useDrawer()
-  const { route } = useRuntime()
+  const { route: { path } } = useRuntime()
+
+  const [url, setUrl] = useState(path)
 
   useEffect(() => {
+    setUrl(path)
+    if (path === url) return
     close()
-  }, [route?.path])
+  }, [path])
 
   return null
 }
